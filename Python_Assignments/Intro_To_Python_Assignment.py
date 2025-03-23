18371902c24f0785b01bb0b28cc7619c4f5718b6
dennis-hub-star <ldmamyala@gmail.com>
2025-03-23 08:16:54 +0200

    Intro to python assignment1(update)

--- Python_Assignments/Intro_To_Python_Assignment.py
+++ Python_Assignments/Intro_To_Python_Assignment.py
@@ -2,6 +2,11 @@ num1 = input("Input the your first number: ")
 operator = input("Input an operator of your choice: ")
 num2 = input("Input the second number: ")
 
+
 results = f"{num1} {operator} {num2}"
 
+# For displaying all the numbers and an operator used
+print(results)    
+
+# Displays the results of the mathematical operation
 print(eval(results))
