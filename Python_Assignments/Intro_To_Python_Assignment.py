8061178fc91be012c91156b7279b38789183a742
dennis-hub-star <ldmamyala@gmail.com>
2025-03-23 08:07:35 +0200

    Intro to python assignment

---
+++ Python_Assignments/Intro_To_Python_Assignment.py
@@ -0,0 +1,7 @@
+num1 = input("Input the your first number: ")
+operator = input("Input an operator of your choice: ")
+num2 = input("Input the second number: ")
+
+results = f"{num1} {operator} {num2}"
+
+print(eval(results))
