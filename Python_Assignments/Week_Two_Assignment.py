4718e41614888f78fb05305bd4f05e52880f6cdf
dennis-hub-star <ldmamyala@gmail.com>
2025-03-23 09:37:54 +0200

    Week Two Assignment

---
+++ Python_Assignments/Week_Two_Assignment.py
@@ -0,0 +1,20 @@
+my_list = []
+
+# Add items
+my_list.append(10)
+my_list.append(30)
+my_list.append(20)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+# Extend
+my_list.extend([50, 60, 70])
+
+# Sort
+my_list.sort()
+print(my_list)
+
+print(my_list.index(30))
+
+
